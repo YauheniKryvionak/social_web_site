f9420d7949b41a80f1db52a1c0fbf64d61f26fc1
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-01-23 18:33:41 +0300

    added email_settings for send email(recommended post)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -147,3 +147,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
